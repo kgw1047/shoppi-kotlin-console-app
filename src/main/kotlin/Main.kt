d89bbf03e31de3674b5fd347cf21587917f0a597
jasonkim <kgw1047@gmail.com>
2022-06-02 19:45:27 +0900

    실습 2. 선택메뉴 표기와 사용자 입력 처리

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -15,4 +15,29 @@ fun main(args: Array<String>) {
         ================================
     """.trimIndent())
 
+    /*
+    Step 1. 상품 카테고리 표기
+    Step 2. 사용자 입력 받기
+    Step 3. 사용자가 기대하는 값을 입력하지 않은 경우에 대한 처리
+     */
+
+    val categories = arrayOf("패션", "전자기기", "반려동물")
+    for (catergory in categories) {
+        println(catergory)
+    }
+
+    println(" => 장바구니로 이동하시려면 #을 입력해주세요")
+
+    var selectedCategory = readLine()
+    while (selectedCategory.isNullOrBlank()){
+        println("값을 다시 입력해주세요")
+        selectedCategory = readLine()
+    }
+    if (selectedCategory == "#"){
+        //TODO 1. 장바구니 이동
+    } else{
+        //TODO 2. 카테고리 상품 목록 보여주기
+        // TODO 3. 카테고리 목록에 없는 값을 입력하는 경우
+    }
+
 }
