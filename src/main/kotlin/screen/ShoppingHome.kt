5f67959ef4191884b3160d2c605ee64cdc2a07d0
jasonkim <kgw1047@gmail.com>
2022-06-06 22:41:19 +0900

    실습 3-1. main 함수의 구현을 클래스의 함수로 분리

---
+++ src/main/kotlin/screen/ShoppingHome.kt
@@ -0,0 +1,47 @@
+package screen
+
+class ShoppingHome {
+
+    fun start() {
+        showWelcomeMessage()
+        showCatergories()
+    }
+
+    private fun showWelcomeMessage() {
+        println("안녕하세요, shoppi에 오신 것을 환영합니다!")
+        println("쇼핑을 계속하시려면 이름을 입력해주세요 :)")
+
+        val name = readLine()
+        println(
+            """
+            감사합니다. 반갑습니다. $name 님
+            원하시는 카테고리를 입력해주세요
+            ================================
+        """.trimIndent()
+        )
+    }
+
+    private fun showCatergories() {
+        val categories = arrayOf("패션", "전자기기", "반려동물")
+        for (catergory in categories) {
+            println(catergory)
+        }
+
+        println(" => 장바구니로 이동하시려면 #을 입력해주세요")
+
+        var selectedCategory = readLine()
+        while (selectedCategory.isNullOrBlank()) {
+            println("값을 다시 입력해주세요")
+            selectedCategory = readLine()
+        }
+        if (selectedCategory == "#") {
+            //TODO 1. 장바구니 이동
+        } else {
+            //TODO 2. 카테고리 상품 목록 보여주기
+            // TODO 3. 카테고리 목록에 없는 값을 입력하는 경리
+        }
+    }
+
+
+
+}
